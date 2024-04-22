e5660af72e986792349e3c6a8af374d25dd1a953
Tristan <tr213190@dal.ca>
2024-04-22 16:34:55 -0300

    initial setup

---
+++ Sendle/src/App.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import './App.css'
+
+function App() {
+
+  return (
+    <Container maxWidth="sm" color="black">
+      <Typography variant="h1" component="h4" sx={{ mb: 2}}>
+        Sendle
+      </Typography>
+    </Container>
+  )
+}
+export default App
